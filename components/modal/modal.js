61cfda47b827cee4827725a6507cdaa8b85600c4
dasharhythm <dasharhythm@gmail.com>
2024-05-27 14:12:18 +0100

    send request to email

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -3,7 +3,7 @@ let overlay = document.querySelector('.modal-overlay');
 if (overlay) {
     overlay.addEventListener("click", function (event) {
         if (event.target === overlay) { // Если клик был на оверлей, закрыть модалку
-            closeModal();
+            closeModal(), closeSuccessModal();
         }
     });
 
@@ -21,4 +21,51 @@ if (overlay) {
         window.scrollTo(0, previousScrollPosition); // Возвращаем позицию прокрутки
         overlay.style.display = 'none'; // Скрываем модалку
     }
+
+    function closeSuccessModal() {
+        document.body.style.overflow = "auto"; // Включаем прокрутку
+        window.scrollTo(0, previousScrollPosition); // Возвращаем позицию прокрутки
+        overlay.style.display = 'none'; // Скрываем модалку
+    }
+
 }
+
+var form = document.getElementById("contact-form");
+var successModal = document.getElementById("success-modal");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            form.reset();
+            showSuccessModal();
+        } else {
+            response.json().then(data => {
+                alert("Ошибка: " + (data.errors ? data.errors.map(error => error.message).join(", ") : "Произошла ошибка при отправке формы."));
+            });
+        }
+    }).catch(error => {
+        alert("Произошла ошибка при отправке формы.");
+    });
+}
+
+function showSuccessModal() {
+    var formModal = document.querySelector('.modal');
+    formModal.classList.add('hidden');
+    successModal.classList.remove('hidden');
+}
+
+function closeSuccessModal() {
+    successModal.classList.add('hidden');
+}
+
+form.addEventListener("submit", handleSubmit);
+
+
